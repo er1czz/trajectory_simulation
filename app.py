93ed247bff7c8b6965541359c5633a54f50092fc
ER1C ZZ <38876045+er1czz@users.noreply.github.com>
2023-10-25 10:09:54 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from dot_gen import dots_df_gen
+import pandas as pd
+
+def main_fn(customer_num, output_name):
+    customer_num = int(customer_num)
+    output_name = str(output_name)
+    df = dots_df_gen(customer_num)
+    output_name = output_name + '.csv'
+    df.to_csv(output_name, index=False)
+    print('number of customer: ', customer_num)
+    print('file has been saved as: ', output_name)
+
+if __name__ == '__main__':
+    customer_num = input('please enter number of customers: ')
+    output_name = input('please enter output file name (withouth .csv): ')
+    main_fn(customer_num, output_name)
